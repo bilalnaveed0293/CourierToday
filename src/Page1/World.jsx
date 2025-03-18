6c3ce027cc2c87358820acf282a86112b917e068
Bilal Naveed <bilalnaveed0293@gmail.com>
2025-03-18 23:35:37 +0500

    Updated hero

--- src/Page1/World.jsx
+++ src/Page1/World.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Mapper from "./Mapper";
 import Service from "./Service.jsx";
 import decor from "../assets/decor.png";
-// components/HeroSection.jsx
-// components/HeroSection.jsx (World.jsx)
+
 const World = () => {
   return (
     <div>
       <div className="mx-auto px-4 md:px-6 lg:pl-24 flex flex-col md:flex-row py-4 md:py-12 relative">
         {/* Left side: Text content */}
-        <div className="w-full md:w-1/2 mb-8 md:mb-0 relative text-center md:text-left">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-2 md:mb-6">
+        <div className="w-full md:w-1/2 mb-4 md:mb-0 relative text-center md:text-left">
+          <h1 className="sm:text-3xl md:text-5xl lg:text-6xl font-bold mb-2 md:mb-6">
             <span className="text-yellow-400">Seamless</span> <span>Cargo Shipping/Courier Service</span>
             <br />
             Anywhere <span className="text-yellow-400">in the</span>
@@ -29,8 +28,8 @@ const World = () => {
           </button>
         </div>
 
-        {/* Right side: World Map */}
-        <div className="w-full md:w-2/3 relative">
+        {/* Right side: World Map - Made bigger for mobile */}
+        <div className="w-full md:w-2/3 relative scale-110 md:scale-100 mt-2 md:mt-0">
           <Mapper />
         </div>
       </div>
